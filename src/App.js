bbb2e731259bbbcf64906f4229fcc72abfdc8863
vanillabranch <lockdown@kakao.com>
2021-11-07 21:31:13 +0900

    useEffect Preview

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-        <h1 className={styles.title}>Welcome back!!!</h1>
-        <Button text={"Continue"}/>
-    </div>
-  );
+    const [counter, setValue] = useState(0);
+    const onClick = () => setValue((prev) => prev + 1);
+    console.log("render");
+    return (
+        <div>
+            <h1>{counter}</h1>
+            <button onClick={onClick}>Click me</button>
+        </div>
+    );
 }
 
 export default App;
